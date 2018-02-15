4805b5d7630def1a8a1fdb2932422b7a1b4ff135
Janice Nam and Michael Fritz <michael@>
2018-02-14 20:44:11 -0800

    add calculator and all js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
-var add = function (number1, number2) {
-	return number1 + number2;
+
+var add = function(add1Input, add2Input) {
+	return add1Input + add2Input;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
-
-var subtract = function(number1, number2) {
-	return number1 - number2;
+var subtract = function(subtract1Input, subtract2Input) {
+	return subtract1Input - subtract2Input;
 };
-alert(subtract(number1, number2));
 
-var multiply = function(number1, number2) {
-	return number1 * number2;
+var multiply = function(multiply1Input, multiply2Input) {
+	return multiply1Input * multiply2Input;
 };
-alert(multiply(number1, number2));
 
-var divide = function(number1, number2) {
-	return number1 / number2;
-};
-alert(divide(number1, number2));
+$(document).ready(function() {
+	$("#addition form").submit(function(event) {
+		event.preventDefault();
+
+		var add1Input = parseInt($("#add1Input").val());
+		var add2Input = parseInt($("#add2Input").val());
+		var result = add(add1Input, add2Input);
+
+		$(".answer").text(result);
+
+	});
+
+	$("#subtraction form").submit(function(event) {
+		event.preventDefault();
+
+		var subtract1Input = parseInt($("#subtract1").val());
+		var subtract2Input = parseInt($("#subtract2").val());
+		var subtractResult = subtract(subtract1Input, subtract2Input);
+
+		$(".difference").text(subtractResult)
+	});
+
+	$("#multiplication form").submit(function(event) {
+		event.preventDefault();
+
+		var multiply1Input = parseInt($("#multiply1").val());
+		var multiply2Input = parseInt($("#multiply2").val());
+		var multiplyResult = multiply(multiply1Input, multiply2Input);
+
+		$(".quotient").text(multiplyResult)
+	});
+
+});
